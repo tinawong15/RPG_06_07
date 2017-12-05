7cee074e82e2b38cb976b5d93a4f88c639609639
tina <tinaw425@gmail.com>
2017-12-05 12:25:01 -0500

    added Protagonist subclasses

---
+++ v2/CookieMonster.java
@@ -0,0 +1,14 @@
+public class CookieMonster extends Monster {
+    
+    public CookieMonster {
+        super();
+        health += 10;
+    }
+
+    public String toString {
+        ans = super();
+        ans += "Type: Cookie Monster" + "\n";
+        return ans;
+    }
+
+}
