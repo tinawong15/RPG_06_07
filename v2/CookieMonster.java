379f3eb2fccf181856a3f48e6e8aedf70e9a6179
Hanna Yang <oopsaloops@Oopsaloops.local>
2017-12-05 12:19:22 -0500

    New Monster subclassses

---
+++ v2/CookieMonster.java
@@ -0,0 +1,14 @@
+public class CookieMonster extends Monster {
+    
+    public CookieMonster {
+        super();
+        health += 10;
+    }
+
+    public ToString {
+        ans = super();
+        ans += "Type: Cookie Monster" + "\n";
+        return ans;
+    }
+
+}
---
+++ v2/Goblin.java
@@ -0,0 +1,7 @@
+public class Goblin extends Monster {
+    
+    public Goblin {
+        super();
+        defense += 10; // goblins have high defense
+    }
+}
