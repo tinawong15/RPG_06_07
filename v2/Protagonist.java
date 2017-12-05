ed495c6fd4cd9d5a4faeabccc7b16b7bb63bfbad
Hanna Yang <oopsaloops@Oopsaloops.local>
2017-12-04 20:38:21 -0500

    made edits to the formatting mostly

--- v2/Protagonist.java
+++ v2/Protagonist.java
@@ -1,33 +1,29 @@
-public class Protagonist extends Character {
-	private String name;
-	private int baseDef; // for normalization
-	private double baseAtk;
-	private boolean isReadyForAtk;
-	
-	public Protagonist(String name){
-		this.name = name;
-		health = 125;
-		strength = 100;
-		defense = 40;
-		attackRating = 0.4;
-		baseDef = 40; // "normal" values
-		baseAtk = 0.4;
-		isReadyForAtk = true;
-	}
+public class Protagonist extends Character{
+    private String name;
+    private int baseDef; // for normalization
+    private double baseAtk;
+    
+    public Protagonist(String name){
+        this.name = name;
+        health = 125;
+        strength = 100;
+        defense = 40;
+        attackRating = 0.4;
+        baseDef = 40; // "normal" values
+        baseAtk = 0.4;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
   
-  public void specialize() {
-    defense -= 10;
-    attackRating += 0.1;
-    isReadyForAtk = true;
-  }
+    public void specialize() {
+        defense -= 10;
+        attackRating += 0.1;
+     }
   
-  public void normalize() {
-    defense = baseDef;
-    attackRating = baseAtk;
-    isReadyForAtk = true;
-  }
+    public void normalize() {
+        defense = baseDef;
+        attackRating = baseAtk;
+     }
 }
