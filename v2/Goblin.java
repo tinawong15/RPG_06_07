d1160bc84f6d954d44f55e26d703eb27d204f40c
Hanna Yang <oopsaloops@Oopsaloops.local>
2017-12-05 12:34:21 -0500

    new subclasses s

---
+++ v2/Goblin.java
@@ -0,0 +1,14 @@
+public class Goblin extends Monster {
+    
+    public Goblin {
+        super();
+        defense += 10;
+    }
+
+    public String toString {
+        ans = super();
+        ans += "Type: Goblin" + "\n";
+        return ans;
+    }
+
+}
---
+++ v2/Slime.java
@@ -0,0 +1,14 @@
+public class Slime extends Monster {
+    
+    public Slime {
+        super();
+        defense += 10;
+    }
+
+    public String toString {
+        ans = super();
+        ans += "Type: Slime" + "\n";
+        return ans;
+    }
+
+}
