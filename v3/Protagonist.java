31a75bfb84d9cba08e7b21b664a34b171e47c12d
tina <tinaw425@gmail.com>
2017-12-13 18:11:56 -0500

    recorded error messages for abstract

--- v3/Protagonist.java
+++ v3/Protagonist.java
@@ -1,4 +1,5 @@
-public class Protagonist extends Character{
+public abstract class Protagonist extends Character{
+    //without naming the class abstract: error: Protagonist is not abstract and does not override abstract method toString() in Protagonist
     protected String name;
     protected int baseDef; // for normalization
     protected double baseAtk;
@@ -27,19 +28,21 @@ public class Protagonist extends Character{
         attackRating = baseAtk;
      }
 
-    public String toString() {
-	String output = "";
-	output += "Protagonist " + name + "'s stats" + "\n"
-	    + "Health: " + health + "\n"
-	    + "Strength:" + strength + "\n"
-	    + "Defense: " + defense + "\n"
-	    + "Attack Rating: " + attackRating + "\n";
-	return output;
-    }
+    public abstract String toString(); 
+ //    {
+	// String output = "";
+	// output += "Protagonist " + name + "'s stats" + "\n"
+	//     + "Health: " + health + "\n"
+	//     + "Strength:" + strength + "\n"
+	//     + "Defense: " + defense + "\n"
+	//     + "Attack Rating: " + attackRating + "\n";
+	// return output;
+ //    }
 
     public static void main(String[] args) {
-	Protagonist james = new Protagonist("Michael");
-	System.out.println(james);
+	//when instantiating a protagonist: error: Protagonist is abstract; cannot be instantiated
+ //    Protagonist james = new Protagonist("Michael");
+	// System.out.println(james);
     }
 	
 	    
